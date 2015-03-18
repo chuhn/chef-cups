4529511b465029e335ca365247923a9ed2c276aa
Christopher Huhn <C.Huhn@gsi.de>
2015-03-18 14:51:14 +0100

    Add cups-lpd setup

---
+++ recipes/cups_lpd.rb
@@ -0,0 +1,17 @@
+#
+# setup cups-lpd gateway for legacy LPD clients
+#
+
+#
+# README.Debian from cups package recommends installing
+#  'update-inetd' and 'cups-bsd'
+#  and enabling cups-lpd by `dpkg-reconfigure cups-bsd`
+#  (relevant debconf setting:   cups-bsd  cups-bsd/setuplpd  boolean true)
+#
+# For now we follow the instructions from the cups-lpd manpage instead:
+
+package 'xinetd'
+
+cookbook_file '/etc/xinetd.d/cups-lpd' do
+  source 'cups-lpd.xinetd'
+end
